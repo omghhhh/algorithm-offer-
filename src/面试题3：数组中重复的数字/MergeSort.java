2be4435ab420648b518edd3b497e50dc4bfd84cb
omghhhh <wavezhh@163.com>
2020-10-19 14:17:25 +0800

    归并排序

--- src/面试题3：数组中重复的数字/MergeSort.java
+++ src/面试题3：数组中重复的数字/MergeSort.java
@@ -1,20 +1,67 @@
+import edu.princeton.cs.algs4.In;
+
+import java.util.Scanner;
+
 /**
  * 归并排序，快速排序，堆排序
  */
 
 public class MergeSort {
-//    归并排序
+    private static Comparable[] aux;  //归并所需的辅助数组
+
+    //比较两个数的大小
+    public static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    //在单行中打印数组
+    public static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + ",");
+        }
+        System.out.println();
+    }
+
+    //将字符串转换为字符数组
+    public static String[] stoSa(String string) {
+        String[] stringArray = string.split(",");
+        return stringArray;
+    }
+
+    //合并子数组
     public static void merge(Comparable[] a, int lo, int mid, int hi) {
         int i = lo, j = mid + 1;
-        Comparable[] aus = new Integer[hi+1];
         for(int k = 0; k <= hi; k++) {
-            aus[k] = a[k];
+            aux[k] = a[k];
         }
         for(int k = lo; k <= hi; k++) {
-            if(i > mid) a[k] = aus[j++];
-            else if(j > hi) a[k] = aus[i++];
-            else if(less(aus[j], aus[i])) a[k] = aus[j++];
-            else a[k] = aus[i++];
+            if(i > mid) a[k] = aux[j++];
+            else if(j > hi) a[k] = aux[i++];
+            else if(less(aux[j], aux[i])) a[k] = aux[j++];
+            else a[k] = aux[i++];
         }
     }
+    //对数组进行排序
+    public static void sort(Comparable[] a) {
+        aux = new Comparable[a.length];
+        sort(a, 0, a.length-1);
+    }
+    public static void sort(Comparable[] a, int lo, int hi) {
+        //将数组a[lo,...,hi]排序
+        if(hi <= lo) return;
+        int mid = (lo + hi)/2;
+        sort(a, lo, mid);
+        sort(a, mid+1, hi);
+        merge(a, lo, mid, hi);
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("请输入需要排序的数字，以“，”隔开：");
+        String s = in.nextLine();
+        String[] a = stoSa(s);
+        sort(a);
+        show(a);
+    }
+
 }
